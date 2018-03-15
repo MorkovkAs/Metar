fbd04b14278d541c2af65db9345201eba7944df4
MorkovkAs <avklimakov@gmail.com>
2018-03-15 18:26:49 +0300

    History and Favorite airports are tabs

--- app/src/main/java/com/antonklimakov/metar/TabbedActivity.java
+++ app/src/main/java/com/antonklimakov/metar/TabbedActivity.java
@@ -1,17 +1,16 @@
 package com.antonklimakov.metar;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.antonklimakov.metar.fragments.AirportFavoriteListFragment;
 import com.antonklimakov.metar.fragments.AirportListFragment;
 
-public class TabbedActivity extends FragmentActivity {
+public class TabbedActivity extends Activity implements ActionBar.TabListener {
 
     AirportListFragment airportListFragment;
     AirportFavoriteListFragment favListFragment;
@@ -21,79 +20,49 @@ public class TabbedActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabbed);
+        ActionBar bar = getActionBar();
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		/*
-         * This is called when orientation is changed.
-		 */
-        if (savedInstanceState != null) {
-            if (savedInstanceState.containsKey("content")) {
-                String content = savedInstanceState.getString("content");
-                if (content.equals(AirportFavoriteListFragment.ARG_ITEM_ID)) {
-                    if (fragmentManager.findFragmentByTag(AirportFavoriteListFragment.ARG_ITEM_ID) != null) {
-                        setFragmentTitle(R.string.app_name);
-                        contentFragment = fragmentManager.findFragmentByTag(AirportFavoriteListFragment.ARG_ITEM_ID);
-                    }
-                }
-            }
-            if (fragmentManager.findFragmentByTag(AirportListFragment.ARG_ITEM_ID) != null) {
-                airportListFragment = (AirportListFragment) fragmentManager.findFragmentByTag(AirportListFragment.ARG_ITEM_ID);
-                contentFragment = airportListFragment;
-            }
-        } else {
-            airportListFragment = new AirportListFragment();
-            setFragmentTitle(R.string.app_name);
-            switchContent(airportListFragment, AirportListFragment.ARG_ITEM_ID);
-        }
+        ActionBar.Tab tab = bar.newTab();
+        tab.setText("History");
+        tab.setTabListener(this);
+        bar.addTab(tab);
+
+        tab = bar.newTab();
+        tab.setText("Favorite");
+        tab.setTabListener(this);
+        bar.addTab(tab);
+
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        if (contentFragment instanceof AirportFavoriteListFragment) {
-            outState.putString("content", AirportFavoriteListFragment.ARG_ITEM_ID);
-        } else {
-            outState.putString("content", AirportListFragment.ARG_ITEM_ID);
-        }
-        super.onSaveInstanceState(outState);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.tabs, menu);
-        return true;
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            case R.id.menu_favorites:
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        switch (tab.getText().toString()) {
+            case "History":
+                setFragmentTitle(R.string.history);
+                airportListFragment = new AirportListFragment();
+                switchContent(airportListFragment, ft, AirportListFragment.ARG_ITEM_ID);
+                break;
+            case "Favorite":
                 setFragmentTitle(R.string.favorite);
                 favListFragment = new AirportFavoriteListFragment();
-                switchContent(favListFragment, AirportFavoriteListFragment.ARG_ITEM_ID);
-                return true;
+                switchContent(favListFragment, ft, AirportFavoriteListFragment.ARG_ITEM_ID);
+                break;
         }
-        return super.onOptionsItemSelected(item);
     }
 
-    public void switchContent(Fragment fragment, String tag) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        while (fragmentManager.popBackStackImmediate()) ;
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+    }
 
-        if (fragment != null) {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.content_frame, fragment, tag);
-            //Only FavoriteListFragment is added to the back stack.
-            if (!(fragment instanceof AirportListFragment)) {
-                transaction.addToBackStack(tag);
-            }
-            transaction.commit();
-            contentFragment = fragment;
-        }
+    public void switchContent(Fragment fragment, FragmentTransaction transaction, String tag) {
+        transaction.replace(R.id.content_frame, fragment, tag);
+        contentFragment = fragment;
     }
 
     protected void setFragmentTitle(int resourseId) {
