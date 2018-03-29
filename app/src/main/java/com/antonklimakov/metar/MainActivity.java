e872a2bfed55b68e8f3a26d8ac35e6bb2aa16775
MorkovkA <avklimakov@gmail.com>
2018-03-29 22:40:42 +0400

    Merge branch 'master' of D:\Developing\AndroidProjects\Metar with conflicts.

--- app/src/main/java/com/antonklimakov/metar/MainActivity.java
+++ app/src/main/java/com/antonklimakov/metar/MainActivity.java
@@ -5,12 +5,14 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.StrictMode;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,13 +20,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.antonklimakov.metar.utils.PrefsName;
+import com.antonklimakov.metar.utils.SavedPreference;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
-
 public class MainActivity extends Activity {
+    SwipeRefreshLayout swipeRefreshLayout;
+
     TextView textViewMetar;
     TextView textViewConditions;
     TextView textViewTemperature;
@@ -37,6 +43,9 @@ public class MainActivity extends Activity {
     TextView textViewWeather;
 
     EditText editTextICAO;
+    String textICAO = "";
+
+    SavedPreference savedPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +54,54 @@ public class MainActivity extends Activity {
         StrictMode.setThreadPolicy(policy);
 
         setContentView(R.layout.activity_main);
+        savedPreference = new SavedPreference();
 
-        textViewMetar = (TextView) findViewById(R.id.textViewMetar);
-        textViewConditions = (TextView) findViewById(R.id.textViewConditions);
-        textViewTemperature = (TextView) findViewById(R.id.textViewTemperature);
-        textViewDewpoint = (TextView) findViewById(R.id.textViewDewpoint);
-        textViewPressure = (TextView) findViewById(R.id.textViewPressure);
-        textViewWinds = (TextView) findViewById(R.id.textViewWinds);
-        textViewVisibility = (TextView) findViewById(R.id.textViewVisibility);
-        textViewCeiling = (TextView) findViewById(R.id.textViewCeiling);
-        textViewClouds = (TextView) findViewById(R.id.textViewClouds);
-        textViewWeather = (TextView) findViewById(R.id.textViewWeather);
+        //  Declare a new thread to do a preference check
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String firstStartOfVersionName = "firstStart-v" + BuildConfig.VERSION_NAME;
+                SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+                boolean isFirstStart = getPrefs.getBoolean(firstStartOfVersionName, true);
 
-        editTextICAO = (EditText) findViewById(R.id.editTextICAO);
+                if (isFirstStart) {
+                    final Intent i = new Intent(MainActivity.this, IntroActivity.class);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            startActivity(i);
+                        }
+                    });
+                    SharedPreferences.Editor e = getPrefs.edit();
+                    e.putBoolean(firstStartOfVersionName, false);
+                    e.apply();
+                }
+            }
+        });
+        t.start();
 
+        swipeRefreshLayout = findViewById(R.id.swipe);
+        swipeRefreshLayout.setColorSchemeResources(R.color.blue_halo, R.color.view_divider_color, R.color.icao);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                refreshMetar();
+            }
+        });
+
+        textViewMetar = findViewById(R.id.textViewMetar);
+        textViewConditions = findViewById(R.id.textViewConditions);
+        textViewTemperature = findViewById(R.id.textViewTemperature);
+        textViewDewpoint = findViewById(R.id.textViewDewpoint);
+        textViewPressure = findViewById(R.id.textViewPressure);
+        textViewWinds = findViewById(R.id.textViewWinds);
+        textViewVisibility = findViewById(R.id.textViewVisibility);
+        textViewCeiling = findViewById(R.id.textViewCeiling);
+        textViewClouds = findViewById(R.id.textViewClouds);
+        textViewWeather = findViewById(R.id.textViewWeather);
+
+        editTextICAO = findViewById(R.id.editTextICAO);
         editTextICAO.addTextChangedListener(new TextWatcher() {
 
             @Override
@@ -67,21 +110,37 @@ public class MainActivity extends Activity {
 
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                //if (s.length() > 0) {
-                //    length = s.length();
-                //}
             }
 
             @Override
             public void afterTextChanged(Editable s) {
                 if (s.length() == 4) {
-                    Log.d("YourTag", "4");
-                    writeMetar(s.toString());
+                    if (!s.toString().equalsIgnoreCase(textICAO)) {
+                        textICAO = s.toString().toUpperCase();
+                    }
+                    writeMetar(textICAO);
+                    invalidateOptionsMenu();
                 }
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                textICAO = data.getStringExtra("icao");
+                refreshMetar();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        invalidateOptionsMenu();
+    }
 
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -95,7 +154,9 @@ public class MainActivity extends Activity {
         outState.putString("textViewCeiling", textViewCeiling.getText().toString());
         outState.putString("textViewClouds", textViewClouds.getText().toString());
         outState.putString("textViewWeather", textViewWeather.getText().toString());
+
         outState.putString("editTextICAO", editTextICAO.getText().toString());
+        outState.putString("ICAO", textICAO);
     }
 
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
@@ -110,14 +171,17 @@ public class MainActivity extends Activity {
         textViewCeiling.setText(savedInstanceState.getString("textViewCeiling"));
         textViewClouds.setText(savedInstanceState.getString("textViewClouds"));
         textViewWeather.setText(savedInstanceState.getString("textViewWeather"));
+
         editTextICAO.setText(savedInstanceState.getString("editTextICAO"));
+        textICAO = savedInstanceState.getString("ICAO");
+        invalidateOptionsMenu();
     }
 
     private void getMetar(String ICAO) {
         Document doc;
         try {
             String link = "http://aviationweather.gov/adds/metars/?chk_metars=ON&hoursStr=most+recent+only&station_ids=" + ICAO + "&submitmet=Get+info&std_trans=translated";
-            doc = Jsoup.connect(link).get();
+            doc = Jsoup.connect(link).timeout(30000).get();
 
             final String metar = doc.select("body > table > tbody > tr:nth-child(3) > td:nth-child(2) > strong").text();
             final String conditions = doc.select("body > table > tbody > tr:nth-child(4) > td:nth-child(2)").text();
@@ -129,6 +193,10 @@ public class MainActivity extends Activity {
             final String ceiling = doc.select("body > table > tbody > tr:nth-child(10) > td:nth-child(2)").text();
             final String clouds = doc.select("body > table > tbody > tr:nth-child(11) > td:nth-child(2)").text();
             final String weather = doc.select("body > table > tbody > tr:nth-child(12) > td:nth-child(2)").text();
+
+            if (metar != null && !metar.equals("")) {
+                savedPreference.addSaved(getBaseContext(), PrefsName.HISTORY, new Airport(textICAO, conditions.substring(conditions.indexOf("(") + 1, conditions.indexOf(")"))));
+            }
 
             runOnUiThread(new Runnable() {
                 @Override
@@ -143,11 +211,19 @@ public class MainActivity extends Activity {
                     textViewCeiling.setText(ceiling);
                     textViewClouds.setText(clouds);
                     textViewWeather.setText(weather);
+                    stopRefreshing();
                 }
             });
 
         } catch (IOException e) {
             e.printStackTrace();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), R.string.failed_to_update, Toast.LENGTH_SHORT).show();
+                }
+            });
+            stopRefreshing();
         }
     }
 
@@ -156,33 +232,37 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 getMetar(ICAO);
+                stopRefreshing();
             }
         });
         myThread.start();
     }
 
-    private boolean refreshMetar() {
-        if(editTextICAO.length() == 4) {
-            Toast.makeText(this, "" + editTextICAO.getText().toString() + " is refreshed", Toast.LENGTH_SHORT).show();
-            writeMetar(editTextICAO.getText().toString());
+    private void refreshMetar() {
+        if (textICAO.length() == 4) {
+            Toast.makeText(this, "" + textICAO + " " + getString(R.string.metar_is_refreshing), Toast.LENGTH_SHORT).show();
+            editTextICAO.setText(textICAO);
+        } else {
+            Toast.makeText(this, R.string.nothing_to_refresh, Toast.LENGTH_SHORT).show();
+            stopRefreshing();
         }
-        else if (textViewMetar.length() == 0)
-            Toast.makeText(this, "Nothing to refresh.", Toast.LENGTH_SHORT).show();
-        else {
-            String findICAO = textViewMetar.getText().toString().substring(0, 4);
-            writeMetar(findICAO);
-            Toast.makeText(this, "" + findICAO + " is refreshed", Toast.LENGTH_SHORT).show();
-        }
-
-        return true;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the main_activity_actions items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean isSaved = savedPreference.isItemSaved(getApplicationContext(), PrefsName.FAVORITE, textICAO);
+
+        menu.getItem(0).setVisible(!isSaved);
+        menu.getItem(1).setVisible(isSaved);
+
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -191,11 +271,36 @@ public class MainActivity extends Activity {
             case R.id.refresh:
                 refreshMetar();
                 return true;
-            case R.id.rate:
+            case R.id.history:
+                Intent intentNew = new Intent(this, TabbedActivity.class);
+                startActivityForResult(intentNew, 1);
+                return true;
+            case R.id.set_bookmark:
+                if (textICAO.length() == 4 && textViewConditions.getText() != null && !textViewConditions.getText().equals("")) {
+                    if (!savedPreference.isItemSaved(getApplicationContext(), PrefsName.FAVORITE, textICAO)) {
+                        savedPreference.addSaved(getBaseContext(), PrefsName.FAVORITE,
+                                new Airport(textICAO, textViewConditions.getText().toString().substring(textViewConditions.getText().toString().indexOf("(") + 1, textViewConditions.getText().toString().indexOf(")"))));
+                        Toast.makeText(this, getString(R.string.add_bookmark), Toast.LENGTH_SHORT).show();
+                    }
+                    invalidateOptionsMenu();
+                } else {
+                    Toast.makeText(this, R.string.nothing_to_add_to_favorites, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case R.id.unset_bookmark:
+                if (savedPreference.isItemSaved(getApplicationContext(), PrefsName.FAVORITE, textICAO)) {
+                    savedPreference.removeSaved(getApplicationContext(), PrefsName.FAVORITE, textICAO);
+                    Toast.makeText(this, getString(R.string.remove_bookmark), Toast.LENGTH_SHORT).show();
+                }
+                invalidateOptionsMenu();
+                return true;
+            case R.id.rate_settings:
                 rateTheApp(this);
                 return true;
-            case R.id.action_settings:
-                //openSettings();
+            case R.id.more_apps:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/developer?id=Anton+Klimakov"));
+                startActivity(intent);
                 return true;
             case R.id.exit:
                 finish();
@@ -205,7 +310,7 @@ public class MainActivity extends Activity {
         }
     }
 
-    public boolean rateTheApp(Context context) {
+    public void rateTheApp(Context context) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(R.string.dialog_about_title);
 
@@ -242,12 +347,20 @@ public class MainActivity extends Activity {
                 Intent email = new Intent(Intent.ACTION_SEND);
                 email.putExtra(Intent.EXTRA_EMAIL, new String[]{"anton.android.apps@gmail.com"});
                 email.setType("message/rfc822");
-                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+                startActivity(Intent.createChooser(email, getString(R.string.choose_mail_client)));
             }
         });
 
         AlertDialog alert = builder.create();
         alert.show();
-        return true;
+    }
+
+    public void stopRefreshing() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 }
